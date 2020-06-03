0a7f1327f4f7abc0b719cc226d678c54d77b334e
Wang SiQian <34145085+wangsiqian@users.noreply.github.com>
2020-06-03 22:15:32 +0800

    添加本地运行的配置文件 (#46)

---
+++ src/configs/local.py
@@ -0,0 +1,20 @@
+from configs import *
+
+STAGE = 'local'
+
+# sanic server config
+HOST = '127.0.0.1'
+PORT = 8088
+WORKERS = 4
+
+# cassandra config
+CASSANDRA_NODES = ['127.0.0.1']
+
+assert CASSANDRA_KEYSPACE != 'sanic_template_example'
+
+# rabbitmq
+RABBITMQ_HOSTNAME = '127.0.0.1'
+RABBITMQ_PORT = 5672
+RABBITMQ_EXCHANGE = 'issue_feedback'
+RABBITMQ_QUEUE = 'issue_feedback'
+RABBITMQ_ROUTING_KEY = 'issue_feedback'
